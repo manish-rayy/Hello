b9cf28d67e6ae79f3bbdc77e664844d41f31b3ff
unknown <babumanish0808@gmail.com>
2022-12-04 01:19:41 +0530

    loginadd

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ let c=0
 let a=19;
 for(i=1;i<=19;i++){
      if(a%i==0){
-     c=c
+     c=c+1
     }
  }
 if(c==2){
@@ -17,16 +17,16 @@ console.log("No")
 2.Palindrome
 let a="masai";
 let b="";
-for(i=a.length-1;i>0;i++){
+for(i=a.length-1;i>0;i--){
     b=b+a[i];
  }
 let c=0
 for(j=0;j<a.length;j++){
      if(a[j]==b[j]){
-       c=c++
+       c=c+1
       }    
 }
-if(c==true){
+if(c>0){
 console.log("Yes")
 }
 else{
