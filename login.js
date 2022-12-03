5486356242bb65446de3ba38ba7c7ca122ce369b
unknown <mkmanishroy28@gmail.com>
2022-12-04 01:04:47 +0530

    logindone

---
+++ login.js
@@ -0,0 +1,33 @@
+1.Prime problem
+let c=0
+let a=19;
+for(i=1;i<=19;i++){
+     if(a%i==0){
+     c=c
+    }
+ }
+if(c==2){
+console.log("Yes")
+}
+else{
+console.log("No")
+}
+
+
+2.Palindrome
+let a="masai";
+let b="";
+for(i=a.length-1;i>0;i++){
+    b=b+a[i];
+ }
+let c=0
+for(j=0;j<a.length;j++){
+     if(a[j]==b[j]){
+       c=c++
+      }    
+}
+if(c==true){
+console.log("Yes")
+}
+else{
+console.log("No")
